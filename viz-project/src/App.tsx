4847f826e3b2726829cb4ac3eed8176e8046c620
Damián Sommer <39548725+Vokyczech@users.noreply.github.com>
2025-05-19 23:00:41 +0200

    Add Material design
    Fastest way to make slider. Expecting usage at different places troughout filters

--- viz-project/src/App.tsx
+++ viz-project/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material';
 import { NetworkVisualization } from './components/NetworkVisualization';
+import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <NetworkVisualization />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <NetworkVisualization />
+      </div>
+    </ThemeProvider>
   );
 }
 
