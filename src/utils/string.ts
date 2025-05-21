e742a096fb5d5a95912a927817245a6779c8d36f
Hanna Hliavitskaya <aglevickaa@gmail.com>
2025-05-22 00:20:54 +0200

    Fix large graph naming

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,12 +1,14 @@
 export const extractInitials = (source: string) =>
   source
-    .split(' ')
+    .split(/[^a-zA-Z0-9]/)
     .map(part =>
       part && part.length > 3
-        ? part[0]
+        ? part
+            .slice(0, 2)
             .split('(')[0]
             .replaceAll(/[^a-zA-Z0-9]/g, '')
             .toUpperCase()
         : part || ''
     )
-    .join('');
+    .join('')
+    .slice(0, 5);
